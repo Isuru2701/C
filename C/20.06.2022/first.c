3283369019a2064af9cafc778130203d834bc63d
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-06-20 10:01:08 +0530

    fixed?

--- C/20.06.2022/first.c
+++ C/20.06.2022/first.c
@@ -2,7 +2,17 @@
 
 int main(void)
 {
+    char value;
 
+    printf("enter value: ");
+    scanf("%c", &value);
+
+    int count = 0;
+    while(count < 8)
+    {
+        printf("%c\n", value);
+        count++;
+    }
     
     return 0;
 }
