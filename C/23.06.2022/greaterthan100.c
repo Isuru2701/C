3247e8399296268e45fd04c79af908a780a6396b
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-06-23 09:46:08 +0530

    greaterthan100.c

---
+++ C/23.06.2022/greaterthan100.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num, sum = 0;
+
+    int counter = 0;
+
+    while (counter < 6)
+    {
+        printf("enter character %i: ", counter + 1);
+        scanf(" %i", &num);
+        if (num > 100)
+        {
+            sum += num;
+        }
+        counter++;
+    }
+
+    printf("sum is: %i", sum);
+    return 0;
+}
