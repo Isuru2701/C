3943a328bf4577ae6a69bc77fc7fdf5fdaa0ace7
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-06-24 11:30:25 +0530

    removed large files

---
+++ C/24.06.2022/functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    printf("%.2f \n", pow(2,7));
+    printf("%.2f \n", ceil(-9.2));
+    printf("%.2f \n", floor(-9.2));
+
+    printf("%.2f \n", floor(7.5));
+    printf("%.2f \n", floor(-10.5));
+    printf("%.2f \n", ceil(-6.4));
+    printf("%.2f \n", pow(6,2));
+
+
+    return 0;
+}
