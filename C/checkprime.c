156c6b621c711c9d08bccd784103ef8b2a74ce5e
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-06-28 19:37:18 +0530

    added checkprime.c

---
+++ C/checkprime.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    printf("enter number to check: ");
+    scanf("%d", &num);
+
+    for (int i = 0; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            
+        }
+    }
+}
