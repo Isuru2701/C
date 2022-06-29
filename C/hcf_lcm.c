9a8da5492a3f6d4c7ce7b78a464536c72295f629
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-06-29 19:24:01 +0530

    first commit for the day

---
+++ C/hcf_lcm.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int LCM(int num1, int num2);
+int HCF(int num1, int num2);
+
+/*
+
+The Least Common Multiple of two or more numbers
+ is the smallest number among all common multiples of the given numbers.
+
+
+
+The Highest Common Factor (HCF) of two numbers is
+ the highest possible number which divides both the numbers completely.
+*/
+
+int main(void)
+{
+
+    int num1, num2;
+    printf("enter number 1: ");
+    scanf("%d", &num1);
+
+    printf("enter number 2: ");
+    scanf("%d", &num2);
+
+    print("LCM is: %d\nHCF is: %d", LCM(num1, num2), HCF(num1, num2));
+
+    return 0;
+}
+/*
+4,5
+
+= 2,5 | 2
+= 2,5 | 2
+= 1,5 | 5
+= 1,1 ---
+=      20
+
+
+*/
+int LCM(int num1, int num2)
+{
+    int lcm = 1, prime;
+    do
+    {
+        
+
+
+    } while (num1 != 1 && num2 != 1);
+}
+
+int HCF(int num1, int num2)
+{
+}
