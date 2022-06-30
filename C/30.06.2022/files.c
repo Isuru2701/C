0bbe2ee1179bb5b0901f0fcc9f8d8edc3cc1eda8
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-06-30 08:49:59 +0530

    filepointer created

--- C/30.06.2022/files.c
+++ C/30.06.2022/files.c
@@ -2,5 +2,20 @@
 
 int main(void)
 {
+
+    //file type pointer - stores memory address of a file loaded to memory
+
+    FILE *filepointer;
+
+    filepointer = fopen("logs.txt", "w");
+                  //fopen("<filename.<filetype>", "<file mode>")
+                  //w - creates file, overwrites any existing file with same name
+                  //r - read
+                  //a - writing at the end of the file
+                  //r+ - update (writing and reading)
+                  //w+ - creates file for update, if if already exits, discards current contents
+                  //a+ - open/create a file for update, written to end of file
+                  
+
     
 }
