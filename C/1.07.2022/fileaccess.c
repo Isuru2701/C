e75b25f2f4c37c9918672561b7f9f71991b9a1f6
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-07-01 18:18:40 +0530

    added fileaccess.c

--- C/1.07.2022/fileaccess.c
+++ C/1.07.2022/fileaccess.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(void)
 {
-    
+    /*
+        a program to enter data: record datetime, user input; name, age, address
+    */
+    struct
     return 0;
 }
