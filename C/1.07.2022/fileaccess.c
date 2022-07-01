a1ebce3f3109cede3f2b0dbaa49ede1f5abe01c3
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-07-01 19:45:15 +0530

    done for the day, probably

--- C/1.07.2022/fileaccess.c
+++ C/1.07.2022/fileaccess.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <time.h>
 
+void ReadFile(char *file);
+
 int main(void)
 {
 
     // a program to enter data: record datetime, user input; name, age, address
+    ReadFile("data.txt");
+    return 0;
+}
 
+void ReadFile(char *file)
+{
     // time management:
 
     struct tm *tptr;
@@ -16,27 +23,20 @@ int main(void)
 
     printf("My name is: %s\n", name);
 
-    filepointer = fopen("data.txt", "a");
+    filepointer = fopen(file, "a");
 
     printf("enter name age and address: ");
     scanf("%s %i %s", name, &age, address);
 
     printf("name: %s | age: %i | address: %s\n", name, age, address);
 
-    //time
+    // time
     t = time(NULL);
     tptr = localtime(&t);
     strftime(buffer, 80, "%d_%m_%Y_%I:%M%p", tptr);
     printf("%s\n", buffer);
 
-    fprintf(filepointer, "%s %s %i %s\n",buffer, name, age, address);
+    fprintf(filepointer, "%s %s %i %s\n", buffer, name, age, address);
 
     fclose(filepointer);
-
-    return 0;
-}
-
-void ReadFile(char *file)
-{
-    
 }
