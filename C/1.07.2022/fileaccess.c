f67536ebd77b895c029af9b238da3835acc6df75
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-07-01 19:03:49 +0530

    added file handling

--- C/1.07.2022/fileaccess.c
+++ C/1.07.2022/fileaccess.c
@@ -3,9 +3,26 @@
 
 int main(void)
 {
-    /*
-        a program to enter data: record datetime, user input; name, age, address
-    */
-    struct
+
+    // a program to enter data: record datetime, user input; name, age, address
+
+    // time management:
+
+    struct tm *tptr;
+    time_t t;
+    FILE *filepointer;
+    char *name, *address;
+    int age;
+
+    filepointer = fopen("data.txt", "w");
+
+    printf("enter name age and address: ");
+    scanf("%s %i %s", &name, &age, &address);
+
+    printf("age is:%i\n", age);
+    t = time(NULL);
+    tptr = localtime(&t);
+    printf("%s", asctime(tptr));
+
     return 0;
 }
