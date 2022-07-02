0f6f0c41330e5b0dcc6cc8f5a132d5fc68dd267b
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-07-02 19:21:19 +0530

    finished fibonacci.c

--- C/2.07.2022/fibonacci.c
+++ C/2.07.2022/fibonacci.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
+/*
+
+    fibonacci sequence: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+                        ^  ^  ^
+                    (n-2) (n-1) (n)
+*/
+
 int main(void)
 {
+    int limit, n = 1, n_1 = 1, n_2 = 0;
 
-    
+    printf("enter maximum number: ");
+    scanf("%d", &limit);
+
+    while (n < limit)
+    {
+        printf("%d,", n);
+        n = n_1 + n_2;
+        n_2 = n_1;
+        n_1 = n;
+    }
+    printf("%d", n);
+
     return 0;
 }
