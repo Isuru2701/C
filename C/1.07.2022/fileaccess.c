27ab6bcdb88683caaed40cdf76856b0b9d7cfb80
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-07-08 09:29:42 +0530

    added ReadFile

--- C/1.07.2022/fileaccess.c
+++ C/1.07.2022/fileaccess.c
@@ -2,12 +2,14 @@
 #include <time.h>
 
 void WriteFile(char *file);
+void ReadFile(char *file);
 
 int main(void)
 {
 
     // a program to enter data: record datetime, user input; name, age, address
     WriteFile("data.txt");
+    ReadFile("data.txt");
     return 0;
 }
 
@@ -24,20 +26,42 @@ void WriteFile(char *file)
     printf("My name is: %s\n", name);
 
     filepointer = fopen(file, "a");
+    if (!filepointer)
+    {
+        printf("file not found");
+    }
+    else
+    {
 
-    printf("enter name age and address: ");
-    scanf("%s %i %s", name, &age, address);
+        printf("enter name age and address: ");
+        scanf("%s %i %s", name, &age, address);
 
-    printf("name: %s | age: %i | address: %s\n", name, age, address);
+        printf("name: %s | age: %i | address: %s\n", name, age, address);
 
-    // time
-    t = time(NULL);
-    tptr = localtime(&t);
-    strftime(buffer, 80, "%d_%m_%Y_%I:%M%p", tptr);
-    printf("%s\n", buffer);
+        // time
+        t = time(NULL);
+        tptr = localtime(&t);
+        strftime(buffer, 80, "%d_%m_%Y_%I:%M%p", tptr);
+        printf("%s\n", buffer);
 
-    fprintf(filepointer, "%s %s %i %s\n", buffer, name, age, address);
+        fprintf(filepointer, "%s %s %i %s\n", buffer, name, age, address);
 
+        fclose(filepointer);
+    }
+}
+
+void ReadFile(char *file)
+{
+    FILE *filepointer;
+    char name[20], address[20], time[20];
+    int age;
+
+    filepointer = fopen(file, "r");
+
+    while (!feof(filepointer))
+    {
+        fscanf(filepointer, "%s %s %i %s", time, name, &age, address);
+        printf("name: %s\t age: %i\t address: %s\t logged at: %s\n", name, age, address, time);
+    }
     fclose(filepointer);
-
 }
