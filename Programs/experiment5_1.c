472aee97a66774f5f024934e827e331a3bd9b4ea
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-07-17 12:12:01 +0530

    done with experiment5.1

--- Programs/experiment5_1.c
+++ Programs/experiment5_1.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 
 /*
-Write a C program to find average of maximum of n positive numbers entered by user. But, 
-if the input is negative, display the average (excluding the average of negative input) and end 
-the program 
+Write a C program to find average of maximum of n positive numbers entered by user. But,
+if the input is negative, display the average (excluding the average of negative input) and end
+the program
 */
 
 int main(void)
 {
-    int n, sum = 0, num;
+    int n, count = 0;
+    float sum = 0, num;
 
     printf("enter number of numbers: ");
     scanf("%d", &n);
 
-    for(int i = 0; i < n; ++i)
+    do
     {
         printf("enter number: ");
-        scanf("%d", &num);
+        scanf("%f", &num);
 
         if (num > 0)
         {
             sum += num;
+            ++count;
         }
         else if (num < 0)
         {
             break;
         }
-    }
-    
+    }while( count < n);
+
+    printf("average of positive numbers (%d): %.2f", count, (sum / count));
+
     return 0;
 }
