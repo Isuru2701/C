4925a0bee0fc4bd58c6e64630d6331438ac14e1d
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-07-17 17:13:24 +0530

    completed experminent 5.2

--- Programs/section3.5/experiment5_2.c
+++ Programs/section3.5/experiment5_2.c
@@ -4,12 +4,12 @@ int main(void)
 {
     float sum = 0;
 
-    for(int i = 1; i <=10; ++i)
+    for (int i = 1; i <= 10; ++i)
     {
-        sum += 1/i;
+        sum += (1 / (float)i);
     }
 
-    printf("sum is %.3f", sum);
+    printf("sum is %.7f", sum);
 
     return 0;
 }
