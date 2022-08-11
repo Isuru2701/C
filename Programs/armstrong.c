9d8a71d4fab94de3dd7c93918c2cf5b41bc8bf8d
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-08-11 07:09:52 +0530

    got the base of it down

--- Programs/armstrong.c
+++ Programs/armstrong.c
@@ -18,11 +18,22 @@ TODO
 */
 int main(void)
 {
-    int num, sum;
-    for (int i = 1; i <= 500;++i)
+    int num, sum, digit;
+    for (int i = 1; i <= 500; ++i)
     {
-        
+        num = i;
+        while (num > 0)
+        {
+            digit = num % 10;               // for example: 457 % 10 = 7,(cuz 457/10 = 45 remainder 7) like that we extract the digit
+            sum += (digit * digit * digit); // cube it and add
+            num = num / 10;                 // divide by 10 to remove the digit, we get rid of it so that in the next iteration the next digit is under scrutiny
+                                            // we don't need to worry about decimal values resulting from this, cuz integers automatically drop anything on the rightside of the decimal point
+        }
+        if (sum == i)
+        {
+            printf("%d", i);
+        }
     }
 
-        return 0;
+    return 0;
 }
