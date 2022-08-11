6e5e4dc30f5143c79f4163a334b79e80a62b49d6
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-08-11 07:01:09 +0530

    miss me?

---
+++ Programs/armstrong.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+armstrong numbers are numbers which equal the sum of their individual digits
+*/
+
+// outputs the armstrong numbers from 1 to 500
+
+/*
+TODO
+[] loop thru 500 numbers
+[] check is armstrong
+    [] while loop
+    [] separate each digit using num%10
+    [] add it's cube to sum
+    [] remove the digit using num/10
+[] IF num == sum output num
+*/
+int main(void)
+{
+    int num, sum;
+    for (int i = 1; i <= 500;++i)
+    {
+        
+    }
+
+        return 0;
+}
