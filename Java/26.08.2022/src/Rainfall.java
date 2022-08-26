4fa13734b65f13e3beddcf8acd215fd152355cc2
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-08-26 09:38:07 +0530

    Rainfall.java

---
+++ Java/26.08.2022/src/Rainfall.java
@@ -0,0 +1,5 @@
+public class Rainfall {
+    public static void main(String[] args) {
+
+    }
+}
--- Java/26.08.2022/src/test.java
+++ Java/26.08.2022/src/test.java
@@ -1,10 +1,28 @@
+import java.util.Arrays;
+
 public class test {
 
     public static void main(String[] args) {
-        float x = 50;
+        //float is by-default taken as double
+        float x = 50.2f;
         boolean y = false;
         int z = 50;
         double a = 50;
+        double[] n = new double[3];
+        while(z == 50)
+        {
+            System.out.println(z);
+            z++;
+
+        }
+
+
+
+        n[0] = 492.4;
+        n[1] = 52;
+        System.out.println(n[0] + n[1]);
+        Arrays.sort(n);
+
         System.out.println(Float.toString(x) + " " + y + " " + Integer.toString(z)+ " " + Double.toString(a));
 
     }
