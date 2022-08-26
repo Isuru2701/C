60b12fffd019e6935a6d2a06dfb0ab24a65c284e
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-08-26 09:08:12 +0530

    all for now

---
+++ Java/26.08.2022/src/test.java
@@ -0,0 +1,11 @@
+public class test {
+
+    public static void main(String[] args) {
+        float x = 50;
+        boolean y = false;
+        int z = 50;
+        double a = 50;
+        System.out.println(Float.toString(x) + " " + y + " " + Integer.toString(z)+ " " + Double.toString(a));
+
+    }
+}
