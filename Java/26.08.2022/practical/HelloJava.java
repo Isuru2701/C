13a298cba7fd88aa63646f9b75cc1633e3a1c2a8
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-08-26 13:05:28 +0530

    Added HelloJava.java

---
+++ Java/26.08.2022/practical/HelloJava.java
@@ -0,0 +1,21 @@
+//Copying from that practical guide >:
+
+/*
+this is my
+first java application
+(apparently)
+ */
+
+class HelloJava {
+    public static void main(String[] args) {
+
+        //experiment 1
+        System.out.println("Hello Java");
+
+        //experiment 2
+        System.out.print("NIBM");
+
+    }
+}
+
+
