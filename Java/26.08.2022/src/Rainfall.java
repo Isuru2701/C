ed4f6b04ce15f613c8e63d0814e64a705600fe9a
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-08-26 09:48:39 +0530

    Rainfall.java done

--- Java/26.08.2022/src/Rainfall.java
+++ Java/26.08.2022/src/Rainfall.java
@@ -1,5 +1,33 @@
+/*
+Write a Java Application to input five rainfall values into an array and output below:
+    - Maximum rainfall value
+    - Minimum rainfall value
+    - Average rainfall value
+ */
+
+import java.util.Scanner;
+
 public class Rainfall {
     public static void main(String[] args) {
 
+        float[] rainfall = new float[5];
+        int max = 0, min = 0;
+        float total = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 5; ++i) {
+
+            System.out.println("enter rainfall value: ");
+            rainfall[i] = sc.nextFloat();
+
+            total += rainfall[i];
+            if (rainfall[i] > rainfall[max]) {
+                max = i;
+            } else if (rainfall[i] < rainfall[min]) {
+                min = i;
+            }
+        }
+        System.out.println("Max rainfall: " + rainfall[max] + " Min rainfall: " + rainfall[min] + "\n Average rainfall: " + total / 5);
     }
 }
