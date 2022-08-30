dc4787919284366a547162b0075fad1bd2f4f70a
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-08-30 14:41:43 +0530

    fixed error in practical2 experiment4

--- Java/practical2/Experiment4/myoop.java
+++ Java/practical2/Experiment4/myoop.java
@@ -1,11 +1,15 @@
 class Student {
     private String name;
+
     public Student(String name) {
-        this.name = nane;
+        this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 }
 
@@ -13,7 +17,7 @@ class OOP {
     public static void main(String[] args) {
         //does not work
         // Student() constructor must be passed a String name argument
-        Student student_1 = new Student();
+        Student student_1 = new Student("Isuru");
         student_1.setName("Amal");
         System.out.println(student_1.getName());
     }
