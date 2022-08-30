666afb4b0b6a0f23fad31877287099165855b239
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-08-30 14:08:29 +0530

    done with experiment13

--- Java/practical3/Experiment11/Experiment2.java
+++ Java/practical3/Experiment11/Experiment2.java
@@ -1,1 +1,23 @@
-        1 file(s) copied.
+class Employee {
+	float basicSalary = 500000;
+
+	public void displayInfo() {
+		System.out.println("My salary is " + basicSalary);
+	}
+
+}
+
+class Manager extends Employee {
+	float allowance = 2000;
+	public void displayInfo() {
+		float totalSalary = basicSalary + allowance;
+		System.out.println("My salary is " + totalSalary);
+	}
+}
+
+class Experiment2 {
+	public static void main(String[] args) {
+		Manager m1 = new Manager();
+		m1.displayInfo();
+	}
+}
---
+++ Java/practical3/Experiment13/Experiment4.java
@@ -0,0 +1,20 @@
+class Animal { 
+	public void displayInfo() {
+		System.out.println("I am  an Animal");
+	}
+
+}
+
+class Dog extends Animal {
+	public void displayInfo() {
+		super.displayInfo();
+		System.out.println("I am a dog");
+	}
+}
+
+class Experiment4 {
+	public static void main(String[] args) {
+		Dog d1 = new Dog();
+		d1.displayInfo();	
+	}
+}
