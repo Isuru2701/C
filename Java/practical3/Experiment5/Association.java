bc34d71e0b6f61d87e0f3e0d2abea45fbe92a83c
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-08-30 13:17:36 +0530

    done till experiment6

--- Java/practical3/Experiment5/Association.java
+++ Java/practical3/Experiment5/Association.java
@@ -32,8 +32,9 @@ class Employee {
 public class Association {
 
 	public static void main(String[] args) {
-		Bank bank = new Employee("Neha");
+		Bank bank = new Bank("Axis");
+		Employee emp = new Employee("Neha");
 
-		System.out.println(emp.getEmployeeName() + " is employee of" + bank.getBankName());
+		System.out.println(emp.getEmployeeName() + " is employee of " + bank.getBankName());
 	}
 }
