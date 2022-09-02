e2aaa538496e299e879261293abcff3aaf92ba3e
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-09-02 09:03:44 +0530

    new duster.java file

---
+++ Java/2.09.2022/duster.java
@@ -0,0 +1,18 @@
+class Duster {
+	
+	private float price;
+	public void setPrice(float p) {
+		price  = p;
+	}
+	public float getPrice() {
+		return price;
+	}
+}
+
+class Main {
+	public static void main(String[] args) {
+		Duster myDuster = new Duster();
+		myDuster.setPrice(400);
+		System.out.println(myDuster.getPrice());
+	}
+}
---
+++ Java/2.09.2022/mic.java
@@ -0,0 +1,10 @@
+class Mic {
+	private String model, color;
+	private float volume;
+}
+
+class Main {
+	public static void main(String[] args) {
+		Mic myMic = new Mic();
+	}
+}
