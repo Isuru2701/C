b76ff72a50045a27aa29fceb6b9ffcc2ddbc0e3c
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-09-06 09:02:37 +0530

    freezing in here

---
+++ Java/6.09.2022/MyApp.java
@@ -0,0 +1,22 @@
+class Keyboard {
+	
+	private String color, model;
+	private int keys;
+
+	public Keyboard() {
+
+	}
+	
+	public void setKeys(int keys) {
+		this.keys = keys;	
+	}
+	
+
+}
+
+class Main {
+	public static void main(String[] args) {
+		Keyboard myKeyboard = new Keyboard();
+
+	}
+}
