2d39d7a8375a3c79f11b342db9dccd9a0c1faec1
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-09-06 11:33:40 +0530

    room.java task complete

--- Java/6.09.2022/room.java
+++ Java/6.09.2022/room.java
@@ -1,11 +1,11 @@
 /*
 
-The XYZ guest house provided rooms to their customers for reservations during last month. The attributes of their rooms given below. Each room has different number, fee and number of days reserved but room reservation fee is same for all the rooms. You are required to write an object-oriented application to perform requested tasks using your own methods and given attributes. Avoid use of the method which performs task iii by methods out of the class.
+The XYZ guest house provided rooms to their customers for reservations last month. The attributes of their rooms are given below. Each room has a different number, fee, and the number of days reserved but the room reservation fee is the same for all the rooms. You are required to write an object-oriented application to perform requested tasks using your own methods and given attributes. Avoid the use of the method which performs task iii by methods out of the class.
 
  Attributes:
 
     room number
-    room fee for a day
+    the room fee for a day
     room reservation fee(1000LKR)
     number of days room reserved
     room income.
@@ -13,18 +13,16 @@ The XYZ guest house provided rooms to their customers for reservations during la
 
 Tasks     :
 
-    Input and set data for number, fee for a day, reservation fee and number of days reserved for three rooms in the guest house.
-    Calculate income of each room.
+    Input and set data for number, fee for a day, reservation fee, and the number of days reserved for three rooms in the guest house.
+    Calculate the income of each room.
     Output income of each room.
  */
-
-import java.util.Scanner;
 
 class Room {
 
     private int num, days;
     private float fee, income;
-    private static float reservation = 1000;
+    private static float reservation = 1000.0f;
 
     public Room(int num, int days, float fee) {
         this.num = num;
@@ -34,9 +32,10 @@ class Room {
 
     public void calIncome() {
         income = (fee * days) + reservation;
+        showIncome();
     }
 
-    public void showIncome() {
+    private void showIncome() {
         System.out.println("Income for room no." + num + " is " + income + "\n");
 
     }
@@ -45,22 +44,16 @@ class Room {
 class RoomMain {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
         int num, days;
         float fee;
 
-        for (int i = 0; i < 3; ++i) {
-            System.out.println("Enter room number: ");
-            num = sc.nextInt();
-            System.out.println("Enter days staying: ");
-            days = sc.nextInt();
-            System.out.println("Enter room fee: ");
-            fee = sc.nextFloat();
-            Room room = new Room(num,days, fee);
+        Room room1 = new Room(1, 5, 1000);
+        Room room2 = new Room(2, 3, 5000);
+        Room room3 = new Room(3, 2, 3000);
 
-            room.calIncome();
-            room.showIncome();
-        }
+        room1.calIncome();
+        room2.calIncome();
+        room3.calIncome();
 
     }
 }
