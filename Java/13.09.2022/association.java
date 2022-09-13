d9425ec05140d41144ca5ca4cca1c710cfe0fb73
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-09-13 09:39:05 +0530

    driver.java stuff

--- Java/13.09.2022/association.java
+++ Java/13.09.2022/association.java
@@ -6,4 +6,43 @@ HAS-A relationship
 Eg: A student has a module
 A driver has a vehicle
 A seller sells a product
+_________           ___________
+|Student|  1  has 1 |  Module |
+|-------|           |---------|
+|-name  |-----------|-duration|
+|-------|    >      |---------|
  */
+
+class Student {
+    private String name;
+    Module module;
+
+    public Student(String name, float duration) {
+        this.name = name;
+        module = new Module(duration);
+    }
+
+    public void showDetails(){
+        System.out.println("Student name: " + name + " Module duration: " + module.getDuration());
+    }
+
+}
+
+class Module {
+    private float duration;
+
+    public Module(float duration) {
+        this.duration = duration;
+    }
+
+    public float getDuration() {
+        return duration;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Student st1 = new Student("Joe", 45);
+        st1.showDetails();
+    }
+}
---
+++ Java/13.09.2022/driver.java
@@ -0,0 +1,42 @@
+class Driver {
+    private int nic;
+    private String name;
+    Vehicle vehicle;
+
+    public Driver(int nic, String name, String model, int number) {
+        this.nic = nic;
+        this.name = name;
+        vehicle = new Vehicle(model, number);
+    }
+
+    public void showDetails() {
+        System.out.println("Driver details:\n NIC: " + nic + "\tName: " + name + "\nVehicle details:\n Model: " + vehicle.getModel() + "\tNumber: " + vehicle.getNumber());
+    }
+}
+
+class Vehicle {
+    private String model;
+    private int number;
+
+    public Vehicle(String model, int number) {
+        this.model = model;
+        this.number = number;
+
+    }
+    public String getModel(){
+        return model;
+    }
+    public int getNumber(){
+        return number;
+    }
+}
+
+class DriverMain {
+    public static void main(String[] args) {
+
+        Driver driver = new Driver(500103, "Joe", "Ford", 0242);
+        driver.showDetails();
+    }
+
+
+}
