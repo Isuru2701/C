e6a9c6b0e09a0742102a052d8b83ecfe79a7e251
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-09-29 10:52:09 +0530

    did some weird stuff in the last commit, ignore that pls

--- Java/29.09.2022/MedYard.java
+++ Java/29.09.2022/MedYard.java
@@ -8,8 +8,9 @@ class Ward {
     }
 
     public void admitPatient(Patient patient) {
-        if (current + 1 < capacity){
+        if (current < capacity){
             System.out.println("Patient " + patient.getPno() + " admitted to ward " + wno);
+            this.current += 1;
         }
         else {
             System.out.println("Ward " + wno + " is full");
@@ -40,10 +41,19 @@ class Patient {
 
 class MedYard {
     public static void main(String[] args) {
-        Ward w1 = new Ward(1, 10, 10);
+        Ward w1 = new Ward(1, 10, 9);
+        Ward w2 = new Ward(2, 5, 4);
         Patient p1 = new Patient(1, "John");
+        Patient p2 = new Patient(2, "Jack");
+        Patient p3 = new Patient(3, "Harry");
+
 
         w1.admitPatient(p1);
+        w1.admitPatient(p2);
+        w1.admitPatient(p3);
+
+        w2.admitPatient(p2);
+        w2.admitPatient(p3);
 
     }
 }
