013f1ab103026ed10f0eba8a2523049cbf43bb88
Isuru Yahampath <isuruyahampath1@gmail.com>
2022-09-29 11:16:43 +0530

    changed the overall schema of MedYard to be a bit more normalized

--- Java/29.09.2022/MedYard.java
+++ Java/29.09.2022/MedYard.java
@@ -7,16 +7,20 @@ class Ward {
         this.current = current;
     }
 
-    public void admitPatient(Patient patient) {
-        if (current < capacity){
-            System.out.println("Patient " + patient.getPno() + " admitted to ward " + wno);
-            this.current += 1;
-        }
-        else {
-            System.out.println("Ward " + wno + " is full");
-        }
+    public int getWno() {
+        return wno;
     }
 
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getCurrent() {
+        return current;
+    }
+    public void setCurrent(int current) {
+        this.current = current;
+    }
 
 
 }
@@ -24,36 +28,37 @@ class Ward {
 class Patient {
     private int pno;
     private String name;
-
-    public int getPno() {
-        return pno;
-    }
-
-    public String getName() {
-        return name;
-    }
+    private Ward ward;
 
     public Patient(int pno, String name) {
         this.pno = pno;
         this.name = name;
     }
+
+    public void admitPatient(Ward ward) {
+        this.ward = ward;
+        if (ward.getCurrent() < ward.getCapacity()) {
+            System.out.println("Patient " + pno + " admitted to ward " + ward.getWno());
+            ward.setCurrent(ward.getCurrent() + 1);
+        } else {
+            System.out.println("Ward " + ward.getWno() + " is full");
+        }
+    }
 }
 
 class MedYard {
     public static void main(String[] args) {
+
+        //dummy data
         Ward w1 = new Ward(1, 10, 9);
         Ward w2 = new Ward(2, 5, 4);
         Patient p1 = new Patient(1, "John");
         Patient p2 = new Patient(2, "Jack");
         Patient p3 = new Patient(3, "Harry");
 
+        p1.admitPatient(w1);
+        p2.admitPatient(w1);
 
-        w1.admitPatient(p1);
-        w1.admitPatient(p2);
-        w1.admitPatient(p3);
-
-        w2.admitPatient(p2);
-        w2.admitPatient(p3);
 
     }
 }
